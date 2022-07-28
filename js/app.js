16080efe299a601210a1ddb8fd05a0a8723b408b
NahuelBarreto04 <nahuelbarreto54@gmail.com>
2022-07-28 01:12:13 -0300

    formjs-lengthErroFuntion

--- js/app.js
+++ js/app.js
@@ -57,21 +57,23 @@ const noEmpty = (name, surname, user, pass) =>
 
 // const nameValid = (name) => name.value.length >= 3;
 
-const allLengthValid = (name, surname, user) =>
+const allLengthValid = (name, surname, user, pass) =>
   inputLength(name, 4, 30) &&
   inputLength(surname, 4, 30) &&
-  inputLength(user, 4, 12);
+  inputLength(user, 4, 12) &&
+  inputLength(pass, 0, 30);
 
 const inputLength = (input, min, max) =>
   input.value.length >= min && input.value.length < max;
 const nameDifSurname = (name, surname) => name.value !== surname.value;
-const userValid = (user) => user.value.length >= 4 && user.value.length < 8;
-const passwordValid = (pass) => {
-  const regExPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
-  return regExPass.test(pass.value);
-};
+// const userValid = (user) => user.value.length >= 4 && user.value.length < 8;
+// const passwordValid = (pass) => {
+//   const regExPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
+//   return regExPass.test(pass.value);
+// };
 
 const validation = (name, surname, user, pass) => {
+  console.log(pass.value);
   // console.log(userValid(user));
   // console.log(allLengthValid(name, surname, user));
   // console.log(nameDifSurname(name, surname));
@@ -79,64 +81,73 @@ const validation = (name, surname, user, pass) => {
   // console.log(search(surname, "surname"));
   // console.log(passwordValid(pass));
   // if (!nameValid(name)) {
-  if (!allLengthValid(name, surname, user)) {
-    lengthError(name, surname, user);
+  if (!allLengthValid(name, surname, user, pass)) {
+    lengthError(name, surname, user, pass);
+    // passError(pass);
+    // alert("malo");
   } else {
+    // validationAll(name, surname, user, pass);
     alert("todo correcto");
   }
-  // }
-  // console.log(allInputsValid(name, surname, user, pass));
-  // if (namesValid(name, surname)) {
-  //   alert("holaaa");
-  // } else {
-  //   alert("nono");
-  // }
 };
-function searchSpan(input, data) {
+const searchSpan = (input, data) => {
   let formElements = input.parentNode.childNodes;
   let formSpans = [...formElements].filter((e) => e.tagName == "SPAN");
   let spanInput = formSpans.find(
     (span) => span.getAttribute("data-input") === data
   );
   return spanInput;
-}
+};
 function inputError(input, mensaje, data) {
   const inputSpan = searchSpan(input, data);
   console.log(inputSpan);
   inputSpan.innerHTML = mensaje;
   input.classList.add("form__error-validationInput");
 }
-function lengthError(name, surname, user) {
-  // const mayor5 = (input,data)=> {
-  //   input >=4 ? inputError(name, "El nombre debe tener minimo 4 caracteres", data): return;
-  // }
-  const nameLenght = name.value.length;
-  const surNameLenght = user.value.length;
-  // console.log(nameLenght);
-  // if (!(nameLenght >= 4)) {
-  //   console.log("asd");
-  //   inputError(name, "El nombre debe tener minimo 4 caracteres");
-  // }
+const lengthError = (name, surname, user, pass) => {
+  const higherMin = (input, mensagge, min, data) => {
+    console.log(input.value.length);
+    if (!(input.value.length >= min)) {
+      return inputError(input, mensagge, data);
+    } else {
+      return validateInput(input, data);
+    }
+  };
+  const minorMax = (input, mensagge, max, data) => {
+    if (!(input.value.length <= max)) {
+      return inputError(input, mensagge, data);
+    }
+  };
+  higherMin(name, "El nombre debe tener minimo 4 caracteres", 4, "name");
+  higherMin(surname, "El nombre debe tener minimo 4 caracteres", 4, "surname");
+  higherMin(user, "El nombre debe tener minimo 4 caracteres", 4, "user");
+  higherMin(
+    pass,
+    "La contraseña debe tener minimo 8 caracteres",
+    8,
+    "password"
+  );
+  minorMax(name, "El nombre debe tener menos 30 caracteres", 30, "name");
+  minorMax(surname, "El nombre debe tener menos 30 caracteres", 30, "surname");
+  minorMax(user, "El nombre debe tener menos 12 caracteres", 12, "user");
+  minorMax(
+    pass,
+    "La contraseña debe tener menos 30 caracteres",
+    30,
+    "password"
+  );
+};
 
-  !(nameLenght >= 4)
-    ? inputError(name, "El nombre debe tener minimo 4 caracteres", "name")
-    : !(nameLenght < 30)
-    ? inputError(name, "El nombre debe tener menos de 30 caracteres", "name")
-    : undefined;
-  !(surNameLenght >= 4)
-    ? inputError(
-        surname,
-        "El apellido debe tener minimo 4 caracteres",
-        "surname"
-      )
-    : !(surNameLenght <= 30)
-    ? inputError(
-        surname,
-        "El apellido debe tener menos de 30 caracteres",
-        "surname"
-      )
-    : console.log("asd");
-}
 function showError() {
   alert("error");
+}
+const validateInput = (input, data) => {
+  const inputSpan = searchSpan(input, data);
+  inputSpan.innerHTML = "";
+  input.classList.remove("form__error-validationInput");
+  input.classList.add("form__validate");
+};
+
+function validationAll(name, surname, user, pass) {
+  console.log("ads");
 }
